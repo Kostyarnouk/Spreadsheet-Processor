f72cc266a98b138ab331345696a07c95548b022e
kostyarnouk@gmail.com <Kostyarnouk@gmail.com>
2024-03-17 14:01:07 -0400

    The completed code

--- SpreadsheetCode.py
+++ SpreadsheetCode.py
@@ -11,7 +11,7 @@ def process_workbook(filename):
     for row in range(2, sheet.max_row + 1):
         cell = sheet.cell(row, 3)
         corrected_price = cell.value * 0.9
-        # add a new column with the corrected prices
+        # adds a new column with the corrected prices
         corrected_price_cell = sheet.cell(row, 4)
         corrected_price_cell.value = corrected_price
 
